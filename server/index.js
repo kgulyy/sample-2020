8dd1f9b6a112ab4c045e4e7f6648db9b0f5cc87c
Sergey Volodin <s.volodin@corp.mail.ru>
2020-02-10 23:30:28 +0300

    lesson-2: done

--- server/index.js
+++ server/index.js
@@ -1,30 +1,102 @@
-const http = require('http');
-const fs = require('fs');
-const debug = require('debug');
+'use strict';
 
-const log = debug('*');
+const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
+const morgan = require('morgan');
+const uuid = require('uuid/v4');
+const path = require('path');
+const app = express();
 
-const server = http.createServer((req, res) => {
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(body.json());
+app.use(cookie());
 
-  const filename = req.url === '/' ? req.url + 'index.html' : req.url;
+const users = {
+  'd.dorofeev@corp.mail.ru': {
+    email: 'd.dorofeev@corp.mail.ru',
+    password: 'password',
+    age: 21,
+    score: 3,
+  },
+  's.volodin@corp.mail.ru': {
+    email: 'marina.titova@corp.mail.ru',
+    password: 'password',
+    age: 21,
+    score: 100500,
+  },
+  'a.ts@corp.mail.ru': {
+    email: 'a.tyuldyukov@corp.mail.ru',
+    password: 'password',
+    age: 21,
+    score: 72,
+  },
+  'a.ostapenko@corp.mail.ru': {
+    email: 'a.ostapenko@corp.mail.ru',
+    password: 'password',
+    age: 21,
+    score: 72,
+  },
+};
+const ids = {};
 
-  fs.readFile(`public${filename}`, (err, body) => {
-    if (err) {
-      log('Error: ', err);
-      res.writeHead(404);
-      res.write('404');
+app.post('/signup', function (req, res) {
+  const password = req.body.password;
+  const email = req.body.email;
+  const age = req.body.age;
+  if (
+      !password || !email || !age ||
+      !password.match(/^\S{4,}$/) ||
+      !email.match(/@/) ||
+      !(typeof age === 'number' && age > 10 && age < 100)
+  ) {
+    return res.status(400).json({error: 'Не валидные данные пользователя'});
+  }
+  if (users[email]) {
+    return res.status(400).json({error: 'Пользователь уже существует'});
+  }
 
-      res.end();
-      return;
-    }
+  const id = uuid();
+  const user = {password, email, age, score: 0};
+  ids[id] = email;
+  users[email] = user;
 
-    log('OK: ', filename);
-
-    res.write(body);
-
-    res.end();
-  });
-
+  res.cookie('podvorot', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+  res.status(201).json({id});
 });
 
-server.listen(3000);
+app.post('/login', function (req, res) {
+  const password = req.body.password;
+  const email = req.body.email;
+  if (!password || !email) {
+    return res.status(400).json({error: 'Не указан E-Mail или пароль'});
+  }
+  if (!users[email] || users[email].password !== password) {
+    return res.status(400).json({error: 'Не верный E-Mail и/или пароль'});
+  }
+
+  const id = uuid();
+  ids[id] = email;
+
+  res.cookie('podvorot', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+  res.status(200).json({id});
+});
+
+app.get('/me', function (req, res) {
+  const id = req.cookies['podvorot'];
+  const email = ids[id];
+  if (!email || !users[email]) {
+    return res.status(401).end();
+  }
+
+  users[email].score += 1;
+
+  res.json(users[email]);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Server listening port ${port}`);
+});
